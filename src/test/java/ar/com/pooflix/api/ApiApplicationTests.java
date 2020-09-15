a2d5c95c015816dc16c0a8de359965f7a366c189
Digneli Davila <digneli14@gmail.com>
2020-09-15 20:30:38 -0300

    primer commit! empezando con el proyecto Pooflix

---
+++ src/test/java/ar/com/pooflix/api/ApiApplicationTests.java
@@ -0,0 +1,13 @@
+package ar.com.pooflix.api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
